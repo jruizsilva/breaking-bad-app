9fed1dece4bcf81f9f0cd3e18f9f784acd92bd69
Jonathan Ruiz Silva <ruizsilvajonathan@gmail.com>
2021-07-20 16:33:42 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Quote from './components/Quote';
+import Spinner from './components/Spinner';
+
+const initialQuote = {
+  text: '',
+  author: '',
+};
 
 function App() {
+  const [quote, setQuote] = useState(initialQuote);
+  const [loading, setLoading] = useState(false);
+
+  const getRandomQuote = async () => {
+    setLoading(true);
+    const url = `https://www.breakingbadapi.com/api/quote/random`;
+    const res = await fetch(url);
+    const [quote] = await res.json();
+    const { quote: text, author } = quote;
+    setQuote({ text, author });
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getRandomQuote();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <img
+        src='https://upload.wikimedia.org/wikipedia/commons/7/77/Breaking_Bad_logo.svg'
+        alt='logo'
+      />
+      <button onClick={getRandomQuote}>Get quote</button>
+      {loading ? <Spinner /> : <Quote quote={quote} />}
     </div>
   );
 }
---
+++ src/components/Quote.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quote = ({ quote: { text, author } }) => {
+  return (
+    <p>
+      {text} <br />
+      <span>- {author}</span>
+    </p>
+  );
+};
+
+export default Quote;
